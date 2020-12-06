e09aa179d85d2a0cd04dff25af6aabb67a15ff7d
kenton7 <63309306+kenton7@users.noreply.github.com>
2020-12-06 16:32:29 +0300

    Initial Commit

---
+++ Messenger/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Messenger
+//
+//  Created by Илья Кузнецов on 06.12.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
