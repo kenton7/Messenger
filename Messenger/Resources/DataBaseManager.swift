c6e480b5f3fcd668ebddb094f2692c78cc6ba829
kenton7 <63309306+kenton7@users.noreply.github.com>
2020-12-26 17:22:01 +0300

    fix email

--- Messenger/Resources/DataBaseManager.swift
+++ Messenger/Resources/DataBaseManager.swift
@@ -21,7 +21,11 @@ extension DatabaseManager {
     //провверка есть ли уже в базе данных такой email или нет
     public func userExists(with email: String,
                            completion: @escaping ((Bool) -> Void)) {
-        database.child(email).observeSingleEvent(of: .value, with: { snapshot in
+        
+        var safeEmail = email.replacingOccurrences(of: ".", with: "-")
+        safeEmail = safeEmail.replacingOccurrences(of: "@", with: "-")
+        
+        database.child(safeEmail).observeSingleEvent(of: .value, with: { snapshot in
             guard snapshot.value as? String != nil else {
                 completion(false)
                 return
@@ -32,7 +36,7 @@ extension DatabaseManager {
     
     /// Inserts new users to database
     public func insertUser(with user: ChatAppUser) {
-        database.child(user.email).setValue([
+        database.child(user.safeEmail).setValue([
             "first_name": user.firstName,
             "last_name": user.lastName
         ])
@@ -43,5 +47,11 @@ struct ChatAppUser {
     let firstName: String
     let lastName: String
     let email: String
+    
+    var safeEmail: String {
+        var safeEmail = email.replacingOccurrences(of: ".", with: "-")
+        safeEmail = safeEmail.replacingOccurrences(of: "@", with: "-")
+        return safeEmail
+    }
     //let profilePictureUrl: String
 }
